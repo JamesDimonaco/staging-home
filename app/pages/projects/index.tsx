1a7d7d170bdf539013ecc70ccea0f936a434d2a7
James Dimonaco <dimonaco.james@gmail.com>
2021-12-07 23:08:22 +0000

    revert changes for test

--- app/pages/projects/index.tsx
+++ app/pages/projects/index.tsx
@@ -5,39 +5,39 @@ import getProjects from "app/projects/queries/getProjects"
 
 const ITEMS_PER_PAGE = 100
 
-// export const ProjectsList = () => {
-//   const router = useRouter()
-//   const page = Number(router.query.page) || 0
-//   const [{ projects, hasMore }] = usePaginatedQuery(getProjects, {
-//     orderBy: { id: "asc" },
-//     skip: ITEMS_PER_PAGE * page,
-//     take: ITEMS_PER_PAGE,
-//   })
+export const ProjectsList = () => {
+  const router = useRouter()
+  const page = Number(router.query.page) || 0
+  const [{ projects, hasMore }] = usePaginatedQuery(getProjects, {
+    orderBy: { id: "asc" },
+    skip: ITEMS_PER_PAGE * page,
+    take: ITEMS_PER_PAGE,
+  })
 
-//   const goToPreviousPage = () => router.push({ query: { page: page - 1 } })
-//   const goToNextPage = () => router.push({ query: { page: page + 1 } })
+  const goToPreviousPage = () => router.push({ query: { page: page - 1 } })
+  const goToNextPage = () => router.push({ query: { page: page + 1 } })
 
-//   return (
-//     <div>
-//       <ul>
-//         {projects.map((project) => (
-//           <li key={project.id}>
-//             <Link href={Routes.ShowProjectPage({ projectId: project.id })}>
-//               <a>{project.name}</a>
-//             </Link>
-//           </li>
-//         ))}
-//       </ul>
+  return (
+    <div>
+      <ul>
+        {projects.map((project) => (
+          <li key={project.id}>
+            <Link href={Routes.ShowProjectPage({ projectId: project.id })}>
+              <a>{project.name}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
 
-//       <button disabled={page === 0} onClick={goToPreviousPage}>
-//         Previous
-//       </button>
-//       <button disabled={!hasMore} onClick={goToNextPage}>
-//         Next
-//       </button>
-//     </div>
-//   )
-// }
+      <button disabled={page === 0} onClick={goToPreviousPage}>
+        Previous
+      </button>
+      <button disabled={!hasMore} onClick={goToNextPage}>
+        Next
+      </button>
+    </div>
+  )
+}
 
 const ProjectsPage: BlitzPage = () => {
   return (
@@ -53,7 +53,9 @@ const ProjectsPage: BlitzPage = () => {
           </Link>
         </p>
 
-        <Suspense fallback={<div>Loading...</div>}>{/* <ProjectsList /> */}</Suspense>
+        <Suspense fallback={<div>Loading...</div>}>
+          <ProjectsList />
+        </Suspense>
       </div>
     </>
   )
